f5b8572f3e8261630fccdcb6e3909fca3c415145
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-04-09 22:49:41 +0200

    chore:initial commit

---
+++ src/main/java/ch/webec/recipeapp/RecipeappApplication.java
@@ -0,0 +1,13 @@
+package ch.webec.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeappApplication.class, args);
+	}
+
+}
