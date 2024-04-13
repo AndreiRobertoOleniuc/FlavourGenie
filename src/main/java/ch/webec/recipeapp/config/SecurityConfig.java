7d0e12876f57585e2c62d88158f3db5b1d2eb8bd
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-04-13 23:01:10 +0200

    dev: setup oauth properties

---
+++ src/main/java/ch/webec/recipeapp/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package ch.webec.recipeapp.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig{
+
+}
