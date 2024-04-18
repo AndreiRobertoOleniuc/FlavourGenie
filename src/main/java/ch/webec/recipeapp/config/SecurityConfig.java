d5a97007e08ecba56b7fb4a42d17b1d7bffb34b2
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-04-18 16:42:14 +0200

    dev: oauth2 google configuration

--- src/main/java/ch/webec/recipeapp/config/SecurityConfig.java
+++ src/main/java/ch/webec/recipeapp/config/SecurityConfig.java
@@ -1,8 +1,22 @@
 package ch.webec.recipeapp.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig{
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth ->
+                    auth.requestMatchers("/").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
 }
