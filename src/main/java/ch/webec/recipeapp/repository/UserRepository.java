5b20c0ed41ca16c2e0b0625c500fd08b615c829c
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-04-18 23:00:45 +0200

    feat: oauth2 user creation

---
+++ src/main/java/ch/webec/recipeapp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package ch.webec.recipeapp.repository;
+
+import ch.webec.recipeapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String>{
+}
