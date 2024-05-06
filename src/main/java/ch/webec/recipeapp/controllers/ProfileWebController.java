a40103744676c40d8dc41f6c5af023df3378e62b
Shenia Scherer <shenia.scherer@students.fhnw.ch>
2024-05-06 12:15:29 +0200

    feat:text fields of user's names and email is shown

---
+++ src/main/java/ch/webec/recipeapp/controllers/ProfileWebController.java
@@ -0,0 +1,15 @@
+package ch.webec.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileWebController {
+
+    @GetMapping("/profile")
+    public String profile(){
+
+        return "profile";
+    }
+
+}
