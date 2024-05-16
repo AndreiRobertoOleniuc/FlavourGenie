cd518442ef95c2518279bff5c38645543c55f9d2
Shenia Scherer <shenia.scherer@students.fhnw.ch>
2024-05-16 15:30:12 +0200

    dev: error handling

--- src/main/java/ch/webec/recipeapp/controllers/RecipeWebController.java
+++ src/main/java/ch/webec/recipeapp/controllers/RecipeWebController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
