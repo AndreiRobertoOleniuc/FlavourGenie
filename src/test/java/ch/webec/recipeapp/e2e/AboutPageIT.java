f8ac65197d24b68ef2aef0e5b190cc5ddb071ded
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-05-24 14:49:26 +0200

    fix: allow E2E to run with both HTMLUnitDriver and ChromeDriver

--- src/test/java/ch/webec/recipeapp/e2e/AboutPageIT.java
+++ src/test/java/ch/webec/recipeapp/e2e/AboutPageIT.java
@@ -1,20 +1,22 @@
 package ch.webec.recipeapp.e2e;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT,properties = {"spring.profiles.active=local"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,properties = {"spring.profiles.active=local"})
 public class AboutPageIT {
     @LocalServerPort
     int port;
 
+    WebDriver driver = new ChromeDriver();
+
     @Test
     void fhnwLink() {
-        var driver = new ChromeDriver();
         driver.navigate().to("http://localhost:%d/".formatted(port));
         var text= "hello";
     }
