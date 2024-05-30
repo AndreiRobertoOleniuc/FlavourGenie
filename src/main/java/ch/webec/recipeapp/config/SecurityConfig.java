8b06d8501c188e754ad0131b4a62894e5c05583d
Andrei Roberto Oleniuc <andrei.oleniuc@apiax.com>
2024-05-30 19:40:42 +0200

    refactor: remove unused imports in AppConfig and SecurityConfig

--- src/main/java/ch/webec/recipeapp/config/SecurityConfig.java
+++ src/main/java/ch/webec/recipeapp/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
